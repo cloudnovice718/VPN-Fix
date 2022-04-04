31057641dd9cfe2ecfc72cb784989e493255a931
cloudnovice718 <81878657+cloudnovice718@users.noreply.github.com>
2022-04-03 20:46:16 -0500

    Added comment explaining purpose of the code

--- Global Protect Fix - PostRebootLauncher.ps1
+++ Global Protect Fix - PostRebootLauncher.ps1
@@ -1,1 +1,2 @@
-﻿Start-Process powershell.exe -Verb RunAs -ArgumentList '-ExecutionPolicy Bypass', '-NoExit', '-NoProfile', '-File "C:\temp\Global Protect Fix\Global Protect Fix - PostReboot.ps1"'
+﻿# Script that launches the post-reboot file
+ Start-Process powershell.exe -Verb RunAs -ArgumentList '-ExecutionPolicy Bypass', '-NoExit', '-NoProfile', '-File "C:\temp\Global Protect Fix\Global Protect Fix - PostReboot.ps1"'
